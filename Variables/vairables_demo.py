33f2f740db24dec05098f076ce7b818141eb26f0
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-04-21 19:09:52 +0300

    practice with variables

--- Variables/vairables_demo.py
+++ Variables/vairables_demo.py
@@ -1,6 +1,18 @@
-name = 'Igor'
-age = 34
-print("=================")
-print("{} Mahon {}".format(name,age))
+# name = 'Igor'
+# age = 34
+# print("=================")
+# print("{} Mahon {}".format(name,age))
+
+# my_string='web driver'
+# print(my_string[0])
+# print(type(my_string))
+# print(len(my_string))
+
+price='$22'
+print(price)
+print(type(price))
+print(int(price[1:])-1)
+
+
 
 
