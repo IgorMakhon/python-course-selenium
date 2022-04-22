aa69f37d09ffd37f1f3c827c446573750809d7bd
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-04-22 21:17:58 +0300

    practice with variables = string

--- Variables/vairables_demo.py
+++ Variables/vairables_demo.py
@@ -8,10 +8,20 @@
 # print(type(my_string))
 # print(len(my_string))
 
-price='$22'
-print(price)
-print(type(price))
-print(int(price[1:])-1)
+# price='$22'
+# print(price)
+# print(type(price))
+# print(int(price[1:])-1)
+
+my_string = 'https://udemy.com/learn'
+print(my_string)
+print(my_string.find('com'))
+print(my_string.startswith('https'))
+print(my_string.isalpha())
+
+abc='new-true-value'
+print(abc.split("-"))
+
 
 
 
