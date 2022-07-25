46e5c52f0bf13edc53954eee4db0028442bd78fc
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-07-25 10:22:22 +0300

    practice with variables = list & dictionaries

---
+++ Data Type/List/list_methods.py
@@ -0,0 +1,17 @@
+countries = ['Egypt', 'Belarus', 'Russia', 'USA', 'Turkey']
+
+#print(type(countries))
+
+# add value in list
+# print(countries.append('Poland'))
+# print(countries)
+
+
+# # delete value from list
+print(countries.remove('Egypt'))
+print(countries)
+print(len(countries))
+
+# another way to remove element from list
+# pint(countries.pop())
+# print(countries)
---
+++ Data Type/Variables/vairables_demo.py
@@ -0,0 +1,39 @@
+# name = 'Igor'
+# age = 34
+# print("=================")
+# print("{} Mahon {}".format(name,age))
+
+# my_string='web driver'
+# print(my_string[0])
+# print(type(my_string))
+# print(len(my_string))
+
+price='$22'
+print(price)
+print(type(price))
+print(int(price[1:3])-1)
+
+#my_string = 'https://udemy.com/learn'
+#print(my_string)
+#print(my_string.find('com'))
+#print(my_string.startswith('https'))
+#print(my_string.isalpha())
+
+#abc='new-true-value'
+#print(abc.split("e"))
+
+#my_var = "I love %s %d" % ("Alla",37)
+#print(my_var)
+
+#my_var2 = "i love {name} and {age}".format(name="Alla",age=37)
+#print(my_var2)
+
+#my_var2 = "i love {1} and {0}".format("Alla",37)
+#print(my_var2)
+
+#prg = "python"
+#my_var2=f"I love {len('sldldl')} times the {prg}"
+#print(my_var2)
+
+
+
