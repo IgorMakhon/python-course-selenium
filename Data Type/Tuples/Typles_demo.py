198712c8a1a78126b5a9969af098a74df15330fb
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-07-26 15:56:43 +0300

    practice with variables = tuple

---
+++ Data Type/Tuples/Typles_demo.py
@@ -0,0 +1,12 @@
+from filecmp import cmp
+
+tuple_a = ('igor')
+print(type(tuple_a))
+print(len(tuple_a))
+print(tuple_a[1])
+print(tuple_a)
+print("------------ MAX")
+print(max(tuple_a))
+print("============ COMPARE")
+# tuple_b = ('alla')
+# cmp(tuple_a, tuple_b)
