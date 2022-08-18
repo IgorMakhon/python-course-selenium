fb6c6b992cdbe77031b9772147db5bf840aa4035
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-08-18 16:55:04 +0300

    practice with nested_else_if

---
+++ Control Flow/nested/nested_if_else.py
@@ -0,0 +1,18 @@
+theatre_name = 'Yanka Kypala'
+age_limit = 18
+
+print(f"Welcome to the {theatre_name}") #py3 only
+print("Welcome to the {}".format(theatre_name)) #works in py2 and py3
+print("Welcome to the " + theatre_name) #works in py2 and py3
+
+user_input = input("Enter your age: ")
+print(f"Your age is {user_input}")
+
+if int(user_input) >= age_limit:
+    print('Enjoy the movie')
+else:
+    adult_present = input("Is another adult with you? yes or no: ")
+    if adult_present.lower() == "yes":
+        print("enjoy the movie")
+    else:
+        print(f'You must be {age_limit} age to watch the movie')
