3a3e3057ee52c6e4857af213734320e8556bce8c
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-08-18 21:18:36 +0300

    practice with BMI_exercise1

--- Control Flow/BMI Course Conditions/BMI exercise.py
+++ Control Flow/BMI Course Conditions/BMI exercise.py
@@ -2,14 +2,18 @@ print("Welcome to BMI calculator!")
 your_weight = input("Enter your weight(kg) - ")
 your_height = input("Enter your height(m) - ")
 
-BMI = float(your_weight)*10000/float(your_height)**2
+BMI = float(your_weight)/float(your_height)**2
 rounded_BMI = round(BMI,1)
-print(rounded_BMI)
+#print(rounded_BMI)
 
-if float(rounded_BMI) <= 18.5:
+if rounded_BMI <= 18.5:
     print(f'Your BMI is {rounded_BMI}. You are Underweight.')
-elif  float(rounded_BMI)> 18.5 and float(rounded_BMI) <= 25:
-    print(f'Your BMI is {rounded_BMI}. You are Underweight.')
+elif  rounded_BMI> 18.5 and float(rounded_BMI) <= 25:
+    print(f'Your BMI is {rounded_BMI}. You are Normal.')
+elif rounded_BMI> 25 and float(rounded_BMI) <= 30:
+    print(f'Your BMI is {rounded_BMI}. You are Overweight.')
+elif rounded_BMI> 30:
+    print(f'Your BMI is {rounded_BMI}. You are Obese.')
 else:
-    print(f'Your BMI is {rounded_BMI}.')
+    print(f'Your BMI is {rounded_BMI}. Not in any category')
 
