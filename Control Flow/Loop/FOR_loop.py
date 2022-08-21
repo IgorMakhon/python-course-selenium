4688984e8cb4972a98307109096586ac17ecd9e1
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-08-21 20:16:24 +0300

    practice for_loop

---
+++ Control Flow/Loop/FOR_loop.py
@@ -0,0 +1,42 @@
+# for i in range(0,5):
+#     print(i)
+
+#example1
+
+# my_list = ["house", "bike", "car", "boat"]
+#
+# for i in my_list:
+#     print(i)
+
+
+# fruits = ["orange", "banana", "apple"]
+# for j in fruits:
+#     print(j)
+
+#example 2
+#print out the word that are 3 or less charcters
+# quote ="Give a man a program, frustrate him for a day.Teach a man to program,frustrate him for a lifetime"
+#
+# for i in quote.split():
+#      if len(i)<=3:
+#         print(i)
+# else:
+#     pass
+
+
+#example 3
+#collect the small words (3 or all less charachters)
+
+# quote ="Give a man a program, frustrate him for a day.Teach a man to program,frustrate him for a lifetime"
+# small_words=[]
+# for i in quote.split():
+#      if len(i)<=3:
+#         small_words.append(i)
+#
+# print(small_words)
+
+#to loop an empty list
+
+empty_list=[]
+for i in empty_list:
+    print(i)
