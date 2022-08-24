9d3b4ecdaede5f7d78865fb1d7bb4a3d1c72da97
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-08-24 19:35:57 +0300

    practice while_loop

---
+++ Control Flow/Loop/WHILE_loop.py
@@ -0,0 +1,13 @@
+#example 1
+# counter=0
+# while counter<10:
+#     counter=counter+1
+#     print(counter)
+
+#example 2
+desirable_number = 10
+user_input = 0
+while user_input != desirable_number:
+    user_input = int(input("Please enter the number from 0 to 10!: "))
+    print(f"You entered : {user_input}")
+print("great!")
