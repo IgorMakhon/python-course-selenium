9cfff43385bb5d20e71cc8e54ef6bbfb317de543
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-09-23 09:56:23 +0300

    practice supersqa + codewars

--- Files/Exercises/generate_random_emails_with_list_of_domains_exercise_v1.py
+++ Files/Exercises/generate_random_emails_with_list_of_domains_exercise_v1.py
@@ -17,5 +17,24 @@ V1:
 - Output a csv file with one email on each line and each line ending with a comma
 - Output file name: out_generate-random_email_with_list_of_domains_v1.csv
 """
+import random
+import string
+# import ipdb
+# ipdb.set_trace()
 
 list_of_domains = ['supersqa.com', 'gmail.com', 'yahoo.com', 'outlook.com', 'msn.com']
+length = 5
+letters = string.ascii_lowercase
+random_string = ''
+email = ''
+list_of_emails = []
+
+for j in range(len(list_of_domains)):
+    for k in range(20):
+        random_string = ''.join(random.choice(letters) for i in range(length))
+        email = random_string + '@' + list_of_domains[j]
+        list_of_emails.append(email)
+
+str_emails = ',\n'.join(list_of_emails)
+with open('./EXERCISE_v1.csv', 'w') as my_f:
+    my_f.write(',\n'.join(list_of_emails))
