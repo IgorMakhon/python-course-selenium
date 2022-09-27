789bb904e943eeb209af68642e98d3071e47210a
IgorMakhon <48486703+IgorMakhon@users.noreply.github.com>
2022-09-27 08:52:28 +0300

    practice supersqa+files

--- Files/Exercises/generate_random_emails_with_list_of_domains_exercise_v2.py
+++ Files/Exercises/generate_random_emails_with_list_of_domains_exercise_v2.py
@@ -12,28 +12,29 @@ import string
 letters = string.ascii_lowercase
 random_string = ''.join(random.choice(letters) for i in range(length))
 
-V1:
-- Create 20 emails for each domain
+V2:
+- Create 100 total emails with domains randomly elected from the list. So the number of emails
+for each domain will be unknown.
 - Output a csv file with one email on each line and each line ending with a comma
-- Output file name: out_generate-random_email_with_list_of_domains_v1.csv
+- Output file name: out_generate_random_email_with_list_of_domains_v2.csv
+
+** the difference from V1 is the domains are random for this one.
 """
+
 import random
 import string
 # import ipdb
 # ipdb.set_trace()
 
 list_of_domains = ['supersqa.com', 'gmail.com', 'yahoo.com', 'outlook.com', 'msn.com']
-email = ''
 list_of_emails = []
 letters = string.ascii_lowercase
 
-#generate the random emails(20 for each domain) and store in string
-for j in range(len(list_of_domains)):
-    for k in range(20):
-        email = ''.join(random.choice(letters) for i in range(5)) + '@' + list_of_domains[j]
-        list_of_emails.append(email)
-#print(type(email))
+#generate 100 random emails with random domians from list and store in string
+for k in range(100):
+    email = ''.join(random.choice(letters) for i in range(5)) + '@' + random.choice(list_of_domains)
+    list_of_emails.append(email)
 
 #save to CSV file with requirements: Output a csv file with one email on each line and each line ending with a comma
-with open('./EXERCISE_v1.csv', 'w') as my_f:
+with open('./EXERCISE_v2.csv', 'w') as my_f:
     my_f.write(',\n'.join(list_of_emails))
